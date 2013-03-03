474b462410c020b498a7f9ce5fe8e945e76f44ae
Maxim Dubinin <sim@gis-lab.info>
2013-03-03 11:49:47 +0400

    remove

--- cgi_test.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-import cgitb ; cgitb.enable()
-import cgi
-
-form = cgi.FieldStorage()
-
-print "Content-Type: text/plain\n" 
-# note the extra blank line terminate the headers
-print form.getvalue("iucncode")
